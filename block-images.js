38c44f1932c55b38541955625236317f647fcb88
Mark Robin <markrobin815@gmail.com>
2019-11-16 06:05:48 +0800

    Add screenshot test sample for blocked images

---
+++ block-images.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+
+  page.on('request', request => {
+    if (request.resourceType() === 'image')
+      request.abort();
+    else
+      request.continue();
+  });
+  await page.goto('https://news.google.com/news/');
+  await page.screenshot({path: 'assets/news.png', fullPage: true});
+
+  await browser.close();
+})();
